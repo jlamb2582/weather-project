ddbc9248e98033375942cb345c6562190d313db3
jlamb2582 <lambj2582@gmail.com>
2022-03-26 23:03:33 -0400

    added basic css styling

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.css';
 import "./App.css";
+import Weather from "./Weather";
 
 
 export default function App() {
   return <div ClassName ="App">
-  Weather App
-  <footer>
-    This project is coded by Jessica Lamb and is {""}
-  <a href = "https://github.com/jlamb2582/weather-project" target ="_blank" rel="noreferrer"> open-sourced on Git hub</a>
+  <Weather />
+  <footer className="footer">
+    This project was coded by Jessica Lamb and is {""}
+  <a href = "https://github.com/jlamb2582/weather-project" target ="_blank" rel="noreferrer"> open-sourced on GitHub</a>
   </footer>
   </div>
 }
