0649e2e7adea7e84bce4a8473e472db9810b9cab
jlamb2582 <lambj2582@gmail.com>
2022-04-09 18:00:42 -0400

    completed weather forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,11 +1,17 @@
-import React, {useState} from "react";
+import React, {useState, useEffect, useLayoutEffect} from "react";
 import "./WeatherForecast.css"
 import axios from "axios";
 import WeatherForecastDay from "./WeatherForecastDay";
+import { cleanup } from "@testing-library/react";
 
 export default function WeatherForecast (props) {
 let [loaded, setLoaded] = useState(false);
 let [forecast, setForecast] = useState(null);
+
+useEffect (() => {
+setLoaded(false);
+}, [props.coordinates])
+
 
 function handleResponse (response) {
 setForecast(response.data.daily);
@@ -18,9 +24,18 @@ if (loaded) {
     return (
         <div className = "WeatherForecast">
         <div className ="row">
-        <div className ="col">
-       <WeatherForecastDay data = {forecast[0]} />
-        </div>
+        {forecast.map(function (dailyForecast, index) {
+          if (index < 5) {
+            return (
+                <div className ="col" key ={index}>
+                <WeatherForecastDay data = {dailyForecast} />
+                 </div>
+             );
+          } else {
+            return null; 
+          }
+          })}
+        
         </div>
         </div>
         );
