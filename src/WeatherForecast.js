ab3f90c0629877c9d9871f73d6cb8b9571653f2a
jlamb2582 <lambj2582@gmail.com>
2022-04-09 18:40:56 -0400

    corrected weather info js

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,8 +1,8 @@
-import React, {useState, useEffect, useLayoutEffect} from "react";
+import React, {useState, useEffect} from "react";
 import "./WeatherForecast.css"
 import axios from "axios";
 import WeatherForecastDay from "./WeatherForecastDay";
-import { cleanup } from "@testing-library/react";
+
 
 export default function WeatherForecast (props) {
 let [loaded, setLoaded] = useState(false);
